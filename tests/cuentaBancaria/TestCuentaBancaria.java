59696af4c71f20bf7ee8c90b77134833c17cdfd8
marvix <castagna.cristian16@gmail.com>
2017-09-07 16:04:13 -0300

    ROJO: Mi Primer rojo (V)_(*,,,*)_(V), caso base , cuenta sin saldo

--- tests/cuentaBancaria/TestCuentaBancaria.java
+++ tests/cuentaBancaria/TestCuentaBancaria.java
@@ -1,5 +1,16 @@
 package cuentaBancaria;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCuentaBancaria {
+	
+	@Test
+	public void creacionDeUnaCuentaSinSaldo(){
+		
+		CuentaBancaria cuenta1 = new CuentaBancaria();
+		
+		Assert.assertEquals(0,cuenta1.getMonto());
+	}
 
 }
